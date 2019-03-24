360de2128003e0ac02c0fe25c8c7bbc1637393f9
mamjerez <correomam@gmail.com>
2019-03-24 20:53:03 +0100

    Pruebas con menu inicial

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
 import { ProcesoComponent } from './components/proceso/proceso.component';
 import { ProcesoDetallesComponent } from './components/proceso-detalles/proceso-detalles.component';
 import { ProcesoAddEditComponent } from './components/proceso-add-edit/proceso-add-edit.component';
 import { ProcesosLinea1Component } from './components/procesos-linea1/procesos-linea1.component';
 
+
+
+
+
 const app_routes: Routes = [
-    { path: 'home', component: ProcesoComponent },
+    { path: 'home', component: AppComponent },
+    { path: 'inicio', component: ProcesoComponent },
     { path: 'detalles', component: ProcesoDetallesComponent },
     { path: 'addedit', component: ProcesoAddEditComponent },
     { path: 'linea1', component: ProcesosLinea1Component },
